8208c697e8e92fbecef01a67bc0a12c4199db0b9
wei.wang <wei.wang@magicwindow.cn>
2018-09-10 19:35:41 +0800

    update

---
+++ Products/MagicWindowUAVSDKBitcode.framework/Headers/MWAPI.h
@@ -0,0 +1,106 @@
+//
+//  MWAPI.h
+//  MerculetSDK
+//
+//  Created by 王伟 on 2018/3/12.
+//  Copyright © 2018年 王伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MWHTTPURLResponse.h"
+
+#define DEPRECATED(_version) __attribute__((deprecated))
+
+/**
+ *  token失效回调回调
+ */
+extern NSString * _Nonnull const MWTokenExpiredNotification;
+
+/// 成功和失败的回调
+typedef void(^MWRealTimeSuccessBlock)(void);
+typedef void(^MWRealTimeFailureBlock)(MWHTTPURLResponse *response);
+
+@interface MWAPI : NSObject
+
+#pragma mark -
+#pragma mark - init
+
+
+/**
+ *  注册app
+ *  需要在 application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions 中调用
+ */
++ (void)registerApp;
+
+/**
+ *  向sdk设置token
+ *  必须是没有失效的token
+ */
++ (void)setToken:(nullable NSString *)token userID:(nullable NSString *)userID;
+
+/**
+ *  取消追踪当前用户
+ *  cancel tracking
+ */
++ (void)cancelUserOpenId;
+
+/**
+ *  show debug
+ */
++ (void)showLogEnable:(BOOL)enable;
+
+
+#pragma mark -
+#pragma mark - action
+
+
+/**
+ *  自定义事件
+ *  @param action 自定义事件的唯一标示，不能为空
+ *  @param attributes 动态参数，最多可包含9个
+ */
++ (void)event:(nonnull NSString *)action
+             attributes:(nullable NSDictionary *)attributes;
+
+/**
+ *  自定义事件，带回调
+ *  @param action 自定义事件的唯一标示，不能为空
+ *  @param attributes   动态参数，最多可包含9个
+ *  @param successBlock 成功的回调
+ *  @param failureBlock 失败的回调
+ */
++ (void)eventRealTime:(nonnull NSString *)action
+             attributes:(nullable NSDictionary *)attributes
+                success:(MWRealTimeSuccessBlock)successBlock
+                failure:(MWRealTimeFailureBlock)failureBlock;
+
+#pragma mark -
+#pragma mark - deprecated
+
+/**
+ *  设置是否是国内API
+ */
++ (void)setChinaEnable:(BOOL)enable __attribute__((deprecated("the method can't play")));
+
+
+/**
+ *  自定义事件
+ *  @param action 自定义事件的唯一标示，不能为空
+ *  @param attributes 动态参数，最多可包含9个
+ */
++ (void)setCustomAction:(nonnull NSString *)action
+             attributes:(nullable NSDictionary *)attributes __attribute__((deprecated("the method can't play")));;
+
+/**
+ *  自定义事件，带回调
+ *  @param action 自定义事件的唯一标示，不能为空
+ *  @param attributes   动态参数，最多可包含9个
+ *  @param successBlock 成功的回调
+ *  @param failureBlock 失败的回调
+ */
++ (void)setCustomAction:(nonnull NSString *)action
+             attributes:(nullable NSDictionary *)attributes
+                success:(MWRealTimeSuccessBlock)successBlock
+        failure:(MWRealTimeFailureBlock)failureBlock __attribute__((deprecated("the method can't play")));;
+
+@end
---
+++ Products/MagicWindowUAVSDKBitcode.framework/Headers/MagicWindowUAVSDKBitcode.h
@@ -0,0 +1,22 @@
+//
+//  MerculetSDK.h
+//  MerculetSDK
+//
+//  Created by 王大吉 on 4/6/2018.
+//  Copyright © 2018 王大吉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MerculetSDK.
+FOUNDATION_EXPORT double MerculetSDKVersionNumber;
+
+//! Project version string for MerculetSDK.
+FOUNDATION_EXPORT const unsigned char MerculetSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MerculetSDK/PublicHeader.h>
+
+#import <MagicWindowUAVSDKBitcode/MWAPI.h>
+#import <MagicWindowUAVSDKBitcode/MerculetEncrypteHelper.h>
+#import <MagicWindowUAVSDKBitcode/MWURLRequestManager.h>
+#import <MagicWindowUAVSDKBitcode/MWHTTPURLResponse.h>
