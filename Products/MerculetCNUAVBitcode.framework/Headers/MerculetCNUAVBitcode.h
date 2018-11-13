e1a9031985acb386bb1a7e764ebcb558d1b8cee8
wei.wang <wei.wang@magicwindow.cn>
2018-11-13 15:55:24 +0800

    update: 修改bitcode版本头文件引入的问题

---
+++ Products/MerculetCNUAVBitcode.framework/Headers/MerculetCNUAVBitcode.h
@@ -0,0 +1,17 @@
+//
+//  MerculetCNUAVBitcode.h
+//  MerculetCNUAVBitcode
+//
+//  Created by wangwei on 2018/11/12.
+//  Copyright © 2018 wangwei. All rights reserved.
+//
+
+#ifndef MerculetCNUAVBitcode_h
+#define MerculetCNUAVBitcode_h
+
+
+#import <MerculetCNUAVBitcode/MWAPI.h>
+#import <MerculetCNUAVBitcode/MWURLRequestManager.h>
+#import <MerculetCNUAVBitcode/MWHTTPURLResponse.h>
+
+#endif /* MerculetCNUAVBitcode_h */
