c9cbf91ae2a83f47fab6c253374b65b2a1efc372
wei.wang <wei.wang@magicwindow.cn>
2018-11-13 20:04:42 +0800

    update: 更改库名

---
+++ Products/MerculetCNSDK.framework/Headers/MerculetCNSDK.h
@@ -0,0 +1,21 @@
+//
+//  MerculetCNSDK.h
+//  MerculetCNSDK
+//
+//  Created by wangwei on 2018/11/13.
+//  Copyright © 2018 wangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MerculetSDK.
+FOUNDATION_EXPORT double MerculetSDKVersionNumber;
+
+//! Project version string for MerculetSDK.
+FOUNDATION_EXPORT const unsigned char MerculetSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MerculetSDK/PublicHeader.h>
+
+#import <MerculetCNSDK/MWAPI.h>
+#import <MerculetCNSDK/MWURLRequestManager.h>
+#import <MerculetCNSDK/MWHTTPURLResponse.h>
---
+++ Products/MerculetCNSDKBitcode.framework/Headers/MerculetCNSDKBitcode.h
@@ -0,0 +1,22 @@
+//
+//  MerculetCNSDKBitcode.h
+//  MerculetCNSDKBitcode
+//
+//  Created by wangwei on 2018/11/13.
+//  Copyright © 2018 wangwei. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MerculetSDK.
+FOUNDATION_EXPORT double MerculetSDKVersionNumber;
+
+//! Project version string for MerculetSDK.
+FOUNDATION_EXPORT const unsigned char MerculetSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MerculetSDK/PublicHeader.h>
+
+#import <MerculetCNSDKBitcode/MWAPI.h>
+#import <MerculetCNSDKBitcode/MWURLRequestManager.h>
+#import <MerculetCNSDKBitcode/MWHTTPURLResponse.h>
